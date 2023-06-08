5eaf41a87d9ecd3f35f99cc94fd0d6e8db04ab2a
juhee <bora2233@naver.com>
2023-06-08 14:26:54 +0900

    feat : @RequestMapping, @GetMapping차이

--- src/main/java/com/lahee/psSocial/lecture/week8/thu/controller/FormController.java
+++ src/main/java/com/lahee/psSocial/lecture/week8/thu/controller/FormController.java
@@ -2,6 +2,8 @@ package com.lahee.psSocial.lecture.week8.thu.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/week8/thu")
 @Slf4j
 public class FormController {
-    @RequestMapping("/send")
+    @GetMapping("/send")
     public String getForm() {
         return "/week8/thu/send";
     }
 
-    @RequestMapping("/receive")
+    @PostMapping("/receive")
     public String receive(@RequestParam("message") String message ,@RequestParam("name") String nm) {
 
         log.info("name :{}, message :{}",nm, message);
